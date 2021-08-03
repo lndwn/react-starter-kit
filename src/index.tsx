b38fbf29fbd99a050df4f76218a2796ff9c82727
Lynden Noye <lynden.noye@getqsic.com>
2021-08-03 12:38:31 +1000

    feat: re-add globalstyle

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,12 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { App } from './views/app'
 import './style/font-face-inter.css'
+import { GlobalStyle } from 'style/global-style'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <GlobalStyle />
   </React.StrictMode>,
   document.getElementById('root')
 )
